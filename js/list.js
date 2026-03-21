0e848e1058b7f6a2ddbe0cec53504bc4ba2403ba
Daniel Krummhauer <d.krummhauer@gmail.com>
2026-03-21 23:31:54 +0100

    feat: Add delete methode at contact list

--- js/list.js
+++ js/list.js
@@ -12,3 +12,13 @@ function renderList(responseToJson) {
 function createList(responseToJson) {
     renderList(responseToJson);
 }
+
+function extractIDs() {
+    // ID extrahieren
+    let ids = Object.values(contactsList).map(contact => Number(contact.id));
+    console.log(ids);
+    // höchste ID bestimmen
+    let nextId = Math.max(...ids) + 1;
+    console.log(nextId);
+    return nextId
+}
--- script.js
+++ script.js
@@ -3,11 +3,17 @@ let contactsList = [];
 let contactEmail = document.getElementById("email");
 let contactName = document.getElementById("name");
 let contactPhone = document.getElementById("phone");
+let contactID = document.getElementById("contact_id");
 
 function init() {
     getData("/contacts"); //direkt auf Kontakte zugreifen
 }
 
 function addContact() {
-    putData();
+    putData("/contacts/");
+}
+
+function deleteContact(id) {
+    deleteData("/contacts/" + id);
+    contactsList = contactsList.filter(contact => contact.id !== id);
 }
