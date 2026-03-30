4cb5134ff6b7c4ad132c4a604cbd477fd028ed1a
J.Hergert <julian.konken@gmx.net>
2026-03-30 22:03:17 +0200

    add load user initials and add log out menu

--- js/login.js
+++ js/login.js
@@ -101,8 +101,8 @@ document.addEventListener("DOMContentLoaded", () => {
       location.href = "register.html"
     )
   );
-
-
+  let userName = 'Guest';
+  localStorage.setItem("userName", userName);
   document.querySelector(".guest-btn")?.addEventListener("click", () =>
     location.href = "../html/summary.html"
   );
